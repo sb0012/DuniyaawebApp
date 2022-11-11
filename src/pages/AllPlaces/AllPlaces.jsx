2d476e511a03aa0c9820f65855994a89c355e824
sb0012 <sb0012.ayconnects@gmail.com>
2022-11-11 12:10:17 +0530

    cancel button remove 11/11/2022

--- src/pages/AllPlaces/AllPlaces.jsx
+++ src/pages/AllPlaces/AllPlaces.jsx
@@ -271,8 +271,8 @@ export default function AllPlaces() {
                                                                                                 {/* Priority */}
                                                                                                 <Form.Group>
                                                                                                     <div className="center-align">
-                                                                                                        <div className="row">
-                                                                                                            <div className="col-md-12 my-2">
+                                                                                                        <div className="row text-dark ">
+                                                                                                            <div className="col-md-12 my-2  ">
                                                                                                                 <label htmlFor="">Priority</label>
                                                                                                             </div>
                                                                                                         </div>
@@ -369,11 +369,11 @@ export default function AllPlaces() {
                                                                                             <Col sm={2}>
                                                                                                 <button type="reset" class="addbtn">Reset</button>
                                                                                             </Col>
-
+{/* 
                                                                                             <Col sm={2}>
                                                                                                 <button type="button" class="addbtn">Cancel</button>
                                                                                             </Col>
-                                                                                            <Col sm={3}></Col>
+                                                                                            <Col sm={3}></Col> */}
                                                                                         </Row>
 
 
